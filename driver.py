bebd881fda8d7662cc3179fe9b1697d6049803dd
alonzno <alonzoa@cs.uoregon.edu>
2018-11-16 10:21:54 -0800

    Rename new_proxy, add driver for spawning processes

--- driver.py
+++ driver.py
@@ -1,3 +1,21 @@
 import os
+import signal
+import proxy
 
-os.system('python3 new_proxy.py')
+def start_proxy():
+    proxy.start_proxy_server("127.0.0.1")
+
+pids = []
+
+if __name__ == "__main__":
+    user_in = ""
+    newpid = os.fork()
+    if newpid == 0:
+        start_proxy()
+    else:
+        pids.append(newpid)
+        while user_in != "quit":
+            user_in = input("Type quit to kill all processes")
+        for pid in pids:
+            os.kill(pid, signal.SIGTERM)
+
