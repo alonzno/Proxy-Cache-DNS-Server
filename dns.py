d576912869f3b430ee120ca3a70d01377f06f4ba
alonzno <alonzoa@cs.uoregon.edu>
2018-11-19 16:50:16 -0800

    Finish DNS

---
+++ dns.py
@@ -0,0 +1,152 @@
+from socket import *
+import json
+
+def read_zone():
+    f = open("zones/www.altamirano.432.zone")
+    data = json.load(f)
+    print(data)
+    return data
+
+zone = read_zone()
+
+def maskToString(b1, b2):
+    return str(bin(ord(b1)&ord(b2)))[2:]
+
+def getRR(data):
+    host_name, q_type = getRRDomain(data)
+    t = ''
+    if (q_type == b'\x00\x01'):
+        t = 'a'
+    try:
+        rr = zone['Records']['.'.join(host_name)]
+        return (rr, host_name);
+    except:
+        print("Not in DNS")
+        return (None, host_name)
+    pass
+
+def getRRDomain(data):
+    scanning = False
+    counter = 0
+    q_len = 0
+    host_name = []
+    domain = ''
+    byte_counter = 0
+    for b in data:
+        if scanning:
+            domain += chr(b)
+            counter += 1;
+            if counter == q_len:
+                host_name.append(domain)
+                domain = ''
+                scanning = False
+                counter = 0
+            if b == 0:
+                break;
+        else:
+            q_len = b
+            scanning = True
+        byte_counter += 1
+
+    q_type = data[byte_counter:byte_counter+2]
+    print(host_name)
+    print(q_type)
+    return (host_name, q_type)
+def questionToBytes(name):
+    retB = b''
+    for p in name:
+        retB += bytes([len(p)])
+        for c in p:
+            retB += ord(c).to_bytes(1, byteorder='big')
+    retB += int(0).to_bytes(1, byteorder='big')
+    retB += int(1).to_bytes(2, byteorder='big')
+    retB += int(1).to_bytes(2, byteorder='big')
+
+    return retB
+
+    #retB += bytes([0])
+
+def recordToBytes(name, type_, value, ttl):
+    retB = b'\xc0\x0c'
+    
+    if type_ == "A":
+        retB += bytes([0]) + bytes([1])
+    retB += bytes([0]) + bytes([1])
+    retB += int(ttl).to_bytes(4, byteorder='big')
+    if type_ == "A":
+        retB += bytes([0]) + bytes([4])
+    for p in value.split('.'):
+        retB += int(p).to_bytes(1, byteorder='big')
+    return retB
+
+    
+
+def extractFlags(data, error = False):
+    byteA = bytes(data[0])
+    byteB = bytes(data[1])
+    
+    qr = '1'
+    opcode = ''
+    for b in range(1,5):
+        opcode += str(ord(byteA) & (1<<b))
+    aa = '1'
+    tc = '0'
+    ra = '0'
+    rd = '0'
+    z = '000'
+    if error:
+        rcode = '0010'
+    else:
+        rcode = '0000'
+
+
+    flags = [qr, opcode, aa, tc, rd, ra, z, rcode]
+    #print("".join(flags))
+    return int("".join(list(flags[0:5])), 2).to_bytes(1, byteorder = 'big') + int("".join(list(flags[5:])), 2).to_bytes(1, byteorder = 'big')
+
+def makeResponse(data):
+    transId = data[0:2]
+    
+    print("Transaction ID")
+    [ print(hex(x)) for x in transId ]
+    
+    flagBytes = data[2:4]
+    flags = extractFlags(flagBytes)
+    
+    qcount = b'\x00\x01'
+    nscount = b'\x00\x00'
+    arcount = b'\x00\x00'
+
+    rr, host_name = getRR(data[12:])
+    if rr == None:
+        acount = b'\x00\x00'
+        flags = extractFlags(flagBytes, True)
+        dnsheader = transId + flags + qcount + acount + nscount + arcount
+        dnsbody = questionToBytes(host_name)
+        return dnsheader + dnsbody
+        #More response forming here
+    else:
+        acount = b'\x00\x01'
+        dnsheader = transId + flags + qcount + acount + nscount + arcount
+        print(dnsheader)
+        dnsbody = questionToBytes(host_name) + recordToBytes(host_name, rr['Type'], rr['Value'], rr['TTL'])
+        return dnsheader + dnsbody
+
+    
+
+def start_dns(ip_to_use):
+    port = 53
+    ip = ip_to_use
+
+    serSock = socket(AF_INET, SOCK_DGRAM)
+    serSock.bind((ip, port))
+    
+
+    while True:
+        data, addr = serSock.recvfrom(512)
+        response = makeResponse(data)
+        serSock.sendto(response, addr)
+        #serSock.sendto(response, addr)
+
+if __name__ == "__main__":
+    start_dns("127.0.0.1")
--- proxy.py
+++ proxy.py
@@ -3,16 +3,17 @@ from re import match
 from socket import *
 import os
 import _thread
-#import sys         #DEBUG
-#import linecache   #DEBUG
+import datetime
 
 def start_proxy_server(ip_to_bind):
     tcpSerSock = socket(AF_INET, SOCK_STREAM)
 
-    tcpSerSock.bind((ip_to_bind, 8888))
+    tcpSerSock.bind((ip_to_bind, 8887))
     tcpSerSock.listen(10)
     path = os.getcwd()
     os.chdir(path + "/cache/")
+
+
 
     def handle_client(tcpCliSock):
         message = str(tcpCliSock.recv(4096))
@@ -32,6 +33,43 @@ def start_proxy_server(ip_to_bind):
         fileToUse = "./cache/" + fullname + "_FILE"
         try:
             f = open("./" + fullname + "_FILE", "rb")
+
+            if filename[0] == '/':
+                hostn = str(filename[1:]).replace("/www.", "", 1).partition("/")[0]
+            else:
+                hostn = filename.replace("/www.", "", 1).partition("/")[0]
+
+            #Handle the case of non port ip:port pattern
+            if match("([0-9]+\.){3}[0-9]+:[0-9]+", hostn):
+                items = hostn.partition(":")
+                ip = items[0]
+                port = int(items[2])
+                addr = (ip, port)
+            else:
+                ip = gethostbyname(hostn)
+                print("IP Address", ip)
+                if ip == "0.0.0.0":
+                    #DNS did not resolve the IP of hostname
+                    return
+                #Connect to website
+                addr = (ip, 80)
+
+
+            c = socket(AF_INET, SOCK_STREAM)
+            c.connect(addr)
+            s = "HEAD "+ "http://" + fullname + " HTTP/1.0\n"
+            s += "Host: " + hostn + "\n\n"
+
+            fileobj = c.makefile("rwb", 256)
+            fileobj.write(s.encode())
+            fileobj.flush()
+            buf = fileobj.read()
+            print(buf.decode())
+
+            statbuf = os.stat("./" + fullname + "_FILE")
+            print("Modification time: {}".format(statbuf.st_mtime))
+            print("Modification datetime: {}".format(datetime.datetime.fromtimestamp(statbuf.st_mtime)))
+
             outputData = f.read()
             fileExist = True
 
@@ -46,13 +84,10 @@ def start_proxy_server(ip_to_bind):
                         hostn = str(filename[1:]).replace("/www.", "", 1).partition("/")[0]
                     else:
                         hostn = filename.replace("/www.", "", 1).partition("/")[0]
-                    #Handle the case of non port ip:port pattern
-                    if match("([0-9]+\.){3}[0-9]+:[0-9]+", hostn):
-                        items = hostn.partition(":")
-                        ip = items[0]
-                        port = int(items[2])
+
+                    def sendGet(addr):
                         c = socket(AF_INET, SOCK_STREAM)
-                        c.connect((ip, port))
+                        c.connect(addr)
                         s = "GET "+ "http://" + fullname + " HTTP/1.0\n"
                         s += "Host: " + hostn + "\n\n"
 
@@ -67,50 +102,30 @@ def start_proxy_server(ip_to_bind):
                             except OSError as err:
                                 if err.errno != errno.EEXIST:
                                     raise
-                        return
+                        return buf
+
+                    #Handle the case of non port ip:port pattern
+                    if match("([0-9]+\.){3}[0-9]+:[0-9]+", hostn):
+                        items = hostn.partition(":")
+                        ip = items[0]
+                        port = int(items[2])
+                        buf = sendGet((ip, port))
                     else:
                         ip = gethostbyname(hostn)
-                    print("IP Address", ip)
-                    if ip == "0.0.0.0":
-                        #DNS did not resolve the IP of hostname
-                        return
-
-                    #Connect to website
-                    c = socket(AF_INET, SOCK_STREAM)
-                    c.connect((ip, 80))
-                    s = "GET "+ "http://" + fullname + " HTTP/1.0\n"
-                    s += "Host: " + hostn + "\n\n"
-
-                    fileobj = c.makefile("rwb", 256)
-                    fileobj.write(s.encode())
-                    fileobj.flush()
-                    buf = fileobj.read()
-
-                    if not os.path.exists(os.path.dirname(fullname)):
-                        try:
-                            os.makedirs(os.path.dirname(fullname))
-                        except OSError as e:
-                            if e.errno != errno.EEXIST:
-                                raise
+                        print("IP Address", ip)
+                        if ip == "0.0.0.0":
+                            #DNS did not resolve the IP of hostname
+                            return
+                        #Connect to website
+                        buf = sendGet((ip, 80))
 
                     #Only Cache objects less that 10MB
                     if len(buf) < 10000000:
                         tmpFile = open("./" + fullname + "_FILE", "wb")
                         tmpFile.write(buf)
+
                     tcpCliSock.send(buf)
                 except Exception as e:
-                    #Begin DEBUG
-                    '''
-                    exc_type, exc_obj, tb = sys.exc_info()
-                    f = tb.tb_frame
-                    lineno = tb.tb_lineno
-                    filename = f.f_code.co_filename
-                    linecache.checkcache(filename)
-                    line = linecache.getline(filename, lineno, f.f_globals)
-                    print('EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj))
-                    print("============================")
-                    '''
-                    #End DEBUG
                     print(e)
                     print(type(e))
                     print(e.args)
@@ -124,3 +139,6 @@ def start_proxy_server(ip_to_bind):
         tcpCliSock, addr = tcpSerSock.accept()
         print("Received a connection from:", addr)
         _thread.start_new_thread( handle_client, (tcpCliSock, ) )
+
+if __name__ == "__main__":
+    start_proxy_server("127.0.0.1")
